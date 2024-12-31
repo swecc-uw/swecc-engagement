c740d57d4ca0436106f603f3f79dcd50edbb1964
Advay Patil <advaypatil27@gmail.com>
2024-12-30 18:33:21 -0800

    Scaffold metrics page

--- src/pages/admin/AdminEngagementPage.tsx
+++ src/pages/admin/AdminEngagementPage.tsx
@@ -1,7 +1,19 @@
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import { Box, Button, Heading, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 interface Props {}
 
 export const AdminEngagementPage: React.FC<Props> = () => {
-  return <>Engagement Page</>;
+  return (
+    <Box>
+      <VStack align={'flex-start'}>
+        <Button as={Link} to="/admin" leftIcon={<ArrowBackIcon />} mb={1}>
+          Go back
+        </Button>
+        <Heading>Engagement Metrics</Heading>
+      </VStack>
+    </Box>
+  );
 };
