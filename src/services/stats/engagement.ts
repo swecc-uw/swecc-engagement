962f58042bd12d8ed2372950f58cc24365f326a0
Advay Patil <advaypatil27@gmail.com>
2024-12-30 20:52:06 -0800

    Add calculation of basic stats

--- src/services/stats/engagement.ts
+++ src/services/stats/engagement.ts
@@ -5,4 +5,47 @@ export interface StatsDisplay {
   value: number;
 }
 
-const getAverageAttendance = (sessions: AttendanceSession[]) => {};
+type StatHandler = (sessions: AttendanceSession[]) => StatsDisplay;
+
+const getAverageAttendance = (sessions: AttendanceSession[]): StatsDisplay => {
+  let sum = 0;
+
+  for (const session of sessions) {
+    sum += session.attendees.length;
+  }
+
+  return { label: 'Average', value: sum / sessions.length };
+};
+
+const getTotalSessionCount = (sessions: AttendanceSession[]): StatsDisplay => {
+  return { label: 'Total Sessions', value: sessions.length };
+};
+
+const minAttendance = (sessions: AttendanceSession[]): StatsDisplay => {
+  return {
+    label: 'Min Attendance',
+    value: sessions.reduce((prev, curr) =>
+      curr.attendees.length < prev.attendees.length ? curr : prev
+    ).attendees.length,
+  };
+};
+
+const maxAttendance = (sessions: AttendanceSession[]): StatsDisplay => {
+  return {
+    label: 'Max Attendance',
+    value: sessions.reduce((prev, curr) =>
+      curr.attendees.length > prev.attendees.length ? curr : prev
+    ).attendees.length,
+  };
+};
+
+export const getStats = (sessions: AttendanceSession[]): StatsDisplay[] => {
+  const statHandlers: StatHandler[] = [
+    getTotalSessionCount,
+    minAttendance,
+    maxAttendance,
+    getAverageAttendance,
+  ];
+
+  return statHandlers.map((statHandler, _) => statHandler(sessions));
+};
